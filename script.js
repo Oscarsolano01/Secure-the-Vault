8ccd020e91e6c9a19e471f8bddd9b74054fc2375
Oscarsolano01 <168662991+Oscarsolano01@users.noreply.github.com>
2024-05-15 10:18:32 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// create variables for each combination
+const numTen= (5 + 5);
+const numForty= (80 / 2);
+const numThirtyNine= (40 - 1);
+
+alert("You have received this message because you have been chosen to open an important vault. Here is the combination: " + numTen + " - " + numForty + " - " + numThirtyNine);
+
